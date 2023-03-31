71e13e6d5289d55a1ac24d42af95e7bd31bd4006
demoreon <demoreon@gmail.com>
2023-03-31 22:18:10 +0100

    Task 6 fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""A  script that:
-- takes in a URL and an email address
-- sends a POST request to the passed URL with the email as a parameter
-- displays the body of the response.
-"""
+""" POST an email #1 """
+
+
 import sys
 import requests
 
