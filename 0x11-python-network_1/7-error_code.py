e542b48883ffe53c3756c9554872ec95df84cafd
demoreon <demoreon@gmail.com>
2023-03-31 22:19:36 +0100

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Error code #1 """
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    link = sys.argv[1]
+
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
