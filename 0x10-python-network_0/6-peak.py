422afb41ae3a19a0cdd74ff87c3e0f846370e618
demoreon <demoreon@gmail.com>
2023-03-31 01:56:28 +0100

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+""" Test function find_peak """
+
+
 def find_peak(list_of_integers):
+    """ Test function find_peak """
+
     if not list_of_integers:
         return None
 
