abbaf6b27c818848449e30df0d927ce1f1f86617
demoreon <demoreon@gmail.com>
2023-04-18 14:09:37 +0100

    printsqu

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,11 +13,10 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if type(size) is not int:
         raise TypeError('size must be an integer')
-    if size < 0 :
+    if size < 0:
         raise ValueError('size must be >= 0')
 
     for i in range(size):
-        for j in range (size):
-            print('#', end = '')
+        for j in range(size):
+            print('#', end='')
         print()
-
