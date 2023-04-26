f584058ffd6ac424a11b75619ad9616f20244228
demoreon <demoreon@gmail.com>
2023-04-26 01:12:23 +0100

    upgrade

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-const link_url = process.argv[2];
+const URL = process.argv[2];
 
-request(link_url, function (err, response, body) {
+request(URL, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
