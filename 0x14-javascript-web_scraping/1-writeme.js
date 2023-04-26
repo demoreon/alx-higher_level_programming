a828327c20d0b15c298033a783667aaa24c51ab9
demoreon <demoreon@gmail.com>
2023-04-26 01:04:47 +0100

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 const file = process.argv[2];
-const s_text = process.argv[3];
+const string = process.argv[3];
 
-fs.writeFile(file, s_text, 'utf-8', function (err) {
+fs.writeFile(file, string, 'utf-8', function (err) {
   if (err) console.log(err);
 });
