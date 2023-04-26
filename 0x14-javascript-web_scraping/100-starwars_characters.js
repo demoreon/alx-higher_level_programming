d169cdaecca4531d373483eee2444e5b5598dba4
demoreon <demoreon@gmail.com>
2023-04-26 01:08:36 +0100

    upgrade

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const req = require('request');
+const i_d = process.argv[2];
+const link_url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(link_url + i_d, function (error, res, body) {
+  if (error) {
+    console.log(error);
+  }
+  const info = JSON.parse(body);
+  const dd = info.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const info1 = JSON.parse(body1);
+      console.log(info1.name);
+    });
+  }
+});
