30927fbb9204f56825c9440aadddc866f152edbf
demoreon <demoreon@gmail.com>
2023-04-27 16:02:39 +0100

    102 rest

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -17,12 +17,13 @@ void print_python_string(PyObject *p)
 	}
 	PyObject *encoded_str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 	char *str = PyBytes_AsString(encoded_str);
+	Py_ssize_t length = PyUnicode_GET_LENGTH(p);
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
 
-	printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
+	printf("  length: %zd\n", length);
 	printf("  value: %s\n", str);
 }
